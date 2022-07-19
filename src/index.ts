5ffe90b7c3d68a2db87a89e731381920ab47f712
Paulius Uosis <paulius.uosis@vialet.eu>
2022-07-19 20:23:59 +0300

    wip: setup express server and initialize endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-/** @module index */
+import express from 'express';
+
+// Server setup
+const PORT = 3000;
+const app = express();
+
+app.get('list', (_, res) => {
+  // TODO return full list of files from state
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({name: 'File_name.jpg', active: true}));
+});
+
+app.patch('scan', (_, res) => {
+  // eslint-disable-next-line max-len
+  // TODO scan defined PATH and update redux state object, marking old files {active: false} if they are not available anymore
+  res.end(JSON.stringify({name: 'File_name.jpg', active: true}));
+});
+
+app.get('download-state', (_, res) => {
+  // TODO download the current existing state object
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({state: '...state'}));
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  // TODO read defined directory files and save it to redux state object
+  console.log(`Express server is running on port ${PORT}`);
+});
