15f9942120c4116479987b1014554c5efe263fe8
Paulius Uosis <paulius.uosis@vialet.eu>
2022-07-23 16:41:23 +0300

    refactor: move routes to a separate module

---
+++ src/routes/root.router.ts
@@ -0,0 +1,49 @@
+import store from '../redux/store';
+import express from 'express';
+
+// eslint-disable-next-line new-cap
+export const rootRouter = express.Router();
+
+// rootRouter.use(express.json());
+
+/**
+ * Returns full list of scanned files
+ */
+rootRouter.get('/list', (_, res) => {
+  try {
+    // TODO return full list of files from state
+    // res.setHeader('Content-Type', 'application/json');
+    // res.end(JSON.stringify({name: 'File_name.jpg', active: true}));
+    // res.end({name: 'File_name.jpg', active: true});
+    const result = {name: 'File_name.jpg', active: true};
+    res.send(result);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+/**
+ * Scans files and saves result to the Redux state object, marking old files
+ * {active: false} if they are not available anymore.
+ */
+rootRouter.patch('/scan', (_, res) => {
+  try {
+    // eslint-disable-next-line max-len
+    // TODO scan defined PATH and update redux state object, marking old files {active: false} if they are not available anymore
+    res.json({name: 'File_name.jpg', active: true});
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+/**
+ * Download the current existing state object
+ */
+rootRouter.get('/download-state', (_, res) => {
+  try {
+    // res.json({name: 'File_name.jpg', active: true});
+    res.json(store.getState());
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,21 @@
 import express from 'express';
-import store from './redux/store';
-import {getDirPathFromEnv, getFilesList} from './reader';
+import {rootRouter} from './routes/root.router';
+import {getDirPathFromEnv, getFilesList} from './services/filesReader';
 
 // Server setup
 const PORT = 3000;
 const app = express();
-let files = [];
 
-/**
- * Returns full list of scanned files
- */
-app.get('/list', (_, res) => {
-  // TODO return full list of files from state
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({name: 'File_name.jpg', active: true}));
-});
-
-/**
- * Scans files and saves result to the Redux state object, marking old files
- * {active: false} if they are not available anymore.
- */
-app.patch('/scan', (_, res) => {
-  // eslint-disable-next-line max-len
-  // TODO scan defined PATH and update redux state object, marking old files {active: false} if they are not available anymore
-  res.json({name: 'File_name.jpg', active: true});
-});
-
-/**
- * Download the current existing state object
- */
-app.get('/download-state', (_, res) => {
-  res.json(store.getState());
-});
+// Send all calls to / to rootRouter
+app.use('/', rootRouter);
 
 // Start the Express server
 app.listen(PORT, () => {
-  // TODO read defined directory files and save it to redux state object
   const directoryPath = getDirPathFromEnv();
+  let files = [];
   files = getFilesList(directoryPath);
+  // TODO save directory files list to redux state object
 
-  console.log(`Express server is running on port ${PORT}`);
+  console.log(`Express server started at http://localhost:${PORT}`);
   console.log(`Initial files list: ${JSON.stringify(files)}`);
 });
